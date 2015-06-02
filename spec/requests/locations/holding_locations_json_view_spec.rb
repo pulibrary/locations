e76a00768256b7c46a078e781649c55eb43c6709
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2015-06-02 13:49:33 -0400

    more fields in views, load PU locations

--- spec/requests/locations/holding_locations_json_view_spec.rb
+++ spec/requests/locations/holding_locations_json_view_spec.rb
@@ -81,5 +81,69 @@ module Locations
       end
 
     end
+
+
+  end
+
+  describe 'HoldingLocation html view', type: :request do
+
+    it 'Renders the html template by default' do
+      get holding_locations_path
+      expect(response).to render_template(:index)
+      expect(response.content_type).to eq 'text/html'
+    end
+
+    describe 'the response body' do
+
+      it "/holding_locations contains expected fields" do
+        2.times { FactoryGirl.create(:holding_location) }
+        expected = []
+        HoldingLocation.all.each do |holding_location|
+          attrs = [
+            CGI::escapeHTML(holding_location.label),
+            holding_location.code,
+            holding_location.aeon_location,
+            holding_location.recap_electronic_delivery_location,
+            holding_location.open,
+            holding_location.requestable,
+            holding_location.always_requestable,
+            holding_location.library.code
+          ]
+          expected << attrs
+        end
+        expected << ['Aeon Location', 'ReCAP Electronic Delivery Location',
+         'Open', 'Requestable', 'Always Requestable']
+        get holding_locations_path
+        expected.flatten.uniq.each {|e| expect(response.body).to include(e.to_s)}
+
+      end
+
+      it "/holding_locations/{code} contains expected fields" do
+        FactoryGirl.create(:library)
+        FactoryGirl.create(:delivery_location)
+        holding_location = FactoryGirl.create(:holding_location)
+        2.times do
+          dl = FactoryGirl.create(:delivery_location)
+          holding_location.delivery_locations << dl
+        end
+        holding_location.reload
+        expected = [
+          CGI::escapeHTML(holding_location.label),
+          holding_location.code,
+          holding_location.aeon_location,
+          holding_location.recap_electronic_delivery_location,
+          holding_location.open,
+          holding_location.requestable,
+          holding_location.always_requestable,
+          holding_location.library.code
+        ]
+        holding_location.delivery_locations.each {
+          |dl| expected << CGI::escapeHTML(dl.label) }
+        get holding_location_path(holding_location)
+        expected.each {|e| expect(response.body).to include(e.to_s)}
+      end
+
+    end
+
   end
 end
