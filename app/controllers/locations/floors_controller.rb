9f0aa1c9d8018eaa2c4771f6fdf6dd79bf882cc2
Shaun Ellis <shaun@sdellis.com>
2016-06-10 17:00:04 -0400

    Adds Floor model, views, controller.

---
+++ app/controllers/locations/floors_controller.rb
@@ -0,0 +1,64 @@
+require_dependency "locations/application_controller"
+
+module Locations
+  class FloorsController < ApplicationController
+    before_action :set_floor, only: [:show, :edit, :update, :destroy]
+
+    # GET /floors
+    def index
+      @floors = Floor.all
+    end
+
+    # GET /floors/1
+    def show
+    end
+
+    # GET /floors/new
+    def new
+      @floor = Floor.new
+    end
+
+    # GET /floors/1/edit
+    def edit
+    end
+
+    # POST /floors
+    def create
+      @floor = Floor.new(floor_params)
+
+      if @floor.save
+        redirect_to @floor, notice: 'Floor was successfully created.'
+      else
+        flash.now[:error] = @floor.errors.full_messages
+        render :new
+      end
+    end
+
+    # PATCH/PUT /floors/1
+    def update
+      if @floor.update(floor_params)
+        redirect_to @floor, notice: 'Floor was successfully updated.'
+      else
+        flash.now[:error] = @floor.errors.full_messages
+        render :edit
+      end
+    end
+
+    # DELETE /floors/1
+    def destroy
+      @floor.destroy
+      redirect_to floors_url, notice: 'Floor was successfully destroyed.'
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_floor
+        @floor = Floor.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def floor_params
+        params.require(:floor).permit(:label, :floor_plan_uri, :starting_point, :walkable_areas, :locations_library_id)
+      end
+  end
+end
---
+++ spec/factories/locations/floors.rb
@@ -0,0 +1,12 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :floor, class: 'Locations::Floor' do
+    label { 'Floor ' + Faker::Number.number(10).to_s }
+    floor_plan_uri { Faker::Internet.url }
+    starting_point "10,10,10,10"
+    walkable_areas "<svg></svg>"
+    library
+  end
+
+end
