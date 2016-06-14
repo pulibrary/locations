d51470a8605ff6e44e851d9becd6f24ec3868b63
Shaun Ellis <shaun@sdellis.com>
2016-06-14 17:13:27 -0400

    fixes library_id problem

--- app/models/locations/floor.rb
+++ app/models/locations/floor.rb
@@ -3,7 +3,5 @@ module Locations
     include Locations::Labeled
     include Locations::WithLibrary
 
-    belongs_to :holding_library, class_name: 'Locations::Library', foreign_key: :holding_library_id
-    
   end
 end
--- db/migrate/20160614154022_add_locations_library_to_locations_floors.rb
+++ db/migrate/20160614154022_add_locations_library_to_locations_floors.rb
@@ -1,5 +1,5 @@
 class AddLocationsLibraryToLocationsFloors < ActiveRecord::Migration
   def change
-    add_reference :locations_floors, :locations_libraries, index: true, foreign_key: true
+    add_reference :locations_floors, :locations_library, index: true, foreign_key: true
   end
 end
