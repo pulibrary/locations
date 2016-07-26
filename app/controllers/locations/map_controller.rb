2bca8c334a3f16321204b1b11c74d1501f5004b8
Shaun Ellis <shaun@sdellis.com>
2016-07-26 15:42:16 -0400

    moves logic out of controller and into Map class

--- app/controllers/locations/map_controller.rb
+++ app/controllers/locations/map_controller.rb
@@ -4,52 +4,19 @@ module Locations
   class MapController < ApplicationController
 
     def index
+        @map = Map.new(map_params)
 
-        if params[:loc] && params[:id]
-          holding_location = get_holding_location(params[:loc])
-          bibrec = get_bibrec(params[:id])
-          if !holding_location.nil? && !bibrec.nil?
-            lib = get_lib(holding_location.locations_library_id)
-            if lib.code == 'firestone'
-              redirect_to  "http://library.princeton.edu/locator/index.php?loc=" + params[:loc]+ "&id=" + params[:id]
-            else
-              redirect_to  "http://princeton.stackmap.com/view/?callno=" + bibrec['call_number_display'].first.gsub!(/\s/,'+') + "&location=" + params[:loc] + "&library=" + lib.label.gsub!(/\s/,'+')
-            end
-          else
-            render plain: "Invalid loc code or bibid", status: 404
-          end
+        if !@map.url.nil?
+          redirect_to @map.url
         else
-          render plain: "No loc code or bibid supplied.", status: 404
+          render plain: "Invalid parameters.", status: 404
         end
 
     end
 
-    private
-
-    def get_holding_location(loc_code)
-      holding_location = Locations::HoldingLocation.find_by(code: loc_code)
-    end
-
-    def get_lib(lib_id)
-      Locations::Library.find(lib_id)
-    end
-
-    def get_bibrec(bibid)
-      url = 'https://bibdata.princeton.edu/bibliographic/' + bibid + '/solr'
-
-      uri = URI.parse(url)
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-
-      request = Net::HTTP::Get.new(uri.request_uri)
-      res = http.request(request)
-
-      if res.code == '200'
-        JSON.parse(res.body)
-      else
-        nil
-      end
-
+    # Only allow a trusted parameter "white list" through.
+    def map_params
+      params.permit(:id, :loc)
     end
 
   end
---
+++ app/models/locations/map.rb
@@ -0,0 +1,56 @@
+module Locations
+  class Map
+    include ActiveModel::Model
+
+    def initialize(params = {})
+      @@locator_url = "http://library.princeton.edu/locator/index.php?"
+      @@stackmap_url = "http://princeton.stackmap.com/view/?"
+      @params = params
+    end
+
+    def url
+      if @params[:loc] && @params[:id]
+        holding_location = get_holding_location(@params[:loc])
+        bibrec = get_bibrec(@params[:id])
+        if !holding_location.nil? && !bibrec.nil?
+          lib = get_lib(holding_location.locations_library_id)
+          if lib.code == 'firestone'
+            @@locator_url + "loc=" + @params[:loc]+ "&id=" + @params[:id]
+          else
+            @@stackmap_url + "callno=" + bibrec['call_number_display'].first.gsub!(/\s/,'+') + "&location=" + @params[:loc] + "&library=" + lib.label.gsub!(/\s/,'+')
+          end
+        else
+          nil
+        end
+      end
+    end
+
+    private
+
+    def get_holding_location(loc_code)
+      holding_location = Locations::HoldingLocation.find_by(code: loc_code)
+    end
+
+    def get_lib(lib_id)
+      Locations::Library.find(lib_id)
+    end
+
+    def get_bibrec(bibid)
+      url = 'https://bibdata.princeton.edu/bibliographic/' + bibid + '/solr'
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+      res = http.request(request)
+
+      if res.code == '200'
+        JSON.parse(res.body)
+      else
+        nil
+      end
+
+    end
+
+  end
+end
